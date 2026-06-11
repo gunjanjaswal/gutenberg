78bd7683f511e3c1d14bd2840588e7ae6f868843
ella <ella@automattic.com>
2026-06-11 15:40:02 +0200

    Revert "Restrict the editable root to canvases in their own document"
    This reverts commit d2fe723695789bb1de0be604c88d35e50feeaa5c.

--- packages/block-editor/src/components/writing-flow/use-selection-observer.js
+++ packages/block-editor/src/components/writing-flow/use-selection-observer.js
@@ -176,9 +176,6 @@ export default function useSelectionObserver() {
 					if (
 						! isMultiSelecting() &&
 						collapsedClientId &&
-						// Only a canvas in its own (iframed) document may
-						// become an editing host.
-						ownerDocument !== document &&
 						hasBlockSupport(
 							getBlockName( collapsedClientId ),
 							'editableRoot',
